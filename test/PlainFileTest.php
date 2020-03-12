c4c2d5badb90e758c37739560af67e2b26a70136
Florian Reuschel <Florian.Reuschel@gmx.de>
2020-03-12 08:55:39 +0100

    Add AppVeyor (#1)

--- test/PlainFileTest.php
+++ test/PlainFileTest.php
@@ -16,6 +16,11 @@ class PlainFileTest extends BaseTest
 
     public function testLinkRead()
     {
+        if (DIRECTORY_SEPARATOR === '\\') {
+            $this->markTestSkipped('Symlinks not available on Windows');
+            return;
+        }
+
         $this->assertSame(
             'hello world',
             $this->xfs->readFile(static::FIXTURES . '/plain-link.txt')
@@ -30,6 +35,11 @@ class PlainFileTest extends BaseTest
 
     public function testDeadLinkRead()
     {
+        if (DIRECTORY_SEPARATOR === '\\') {
+            $this->markTestSkipped('Symlinks not available on Windows');
+            return;
+        }
+
         $this->expectException(FileNotFoundException::class);
         $this->xfs->readFile(static::FIXTURES . '/dead-link.txt');
     }
